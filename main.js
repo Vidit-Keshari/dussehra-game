d54ebd5ce19846320035d4090eb56e4fb51daf64
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2024-11-30 17:55:34 +0530

    Major Update 1.3, update html, css, and js
    Add new features in game.

--- main.js
+++ main.js
@@ -450,3 +450,7 @@ function repairFire() {
 function gmail() {
     window.open('https://mail.google.com/mail/?view=cm&fs=1&su=Hey!+I+have+found+a+bug&to=viditvihaan@gmail.com&body=The+bug+is-');
 }
+ 
+function feedback() {
+    window.open('https://forms.gle/zusrokzTPUAR1rhJ9');
+}
